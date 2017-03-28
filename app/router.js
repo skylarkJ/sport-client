b3f7855fb4322f3f981fd7e57d5db8a5fafb29a0
skylarkJ <janaryndin@gmail.com>
2017-03-28 12:07:30 -0400

    Fix Favorites, Add to Navbar, Search in Index
    Fixed favorites - needed to be js as a file located in routes.
    Added favorites to navbar. Got rid off component-search from my
    application and put it into the main index.

--- app/router.js
+++ app/router.js
@@ -10,7 +10,7 @@ Router.map(function () {
   this.route('sign-in');
   this.route('change-password');
   this.route('users');
-  this.route('favorites.js');
+  this.route('favorites');
 });
 
 export default Router;

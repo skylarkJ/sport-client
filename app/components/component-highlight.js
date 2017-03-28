149ec2966c39036932d376c880c273a5ae595d40
skylarkJ <janaryndin@gmail.com>
2017-03-27 20:56:31 -0400

    Add Highlight to Favorites and Handlebars
    Added highlight to favorites with flash message.
    Added handlebars if statement for user only to be able to add a highlight
    to favorites.

--- app/components/component-highlight.js
+++ app/components/component-highlight.js
@@ -1,4 +1,27 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  store: Ember.inject.service(),
+  auth: Ember.inject.service(),
+  isHidden: false,
+  flashMessages: Ember.inject.service(),
+  isAuthenticated: Ember.computed.alias('auth.isAuthenticated'),
+  actions: {
+    addToFavorites (highlight) {
+      this.get('store').createRecord('favorite', {
+        title: highlight.snippet.data.title,
+        date: highlight.snippet.data.publishedAt,
+        video_id: highlight.id,
+        image: highlight.snippet.data.thumbnails.default.url
+      })
+      .save()
+      .then(() => {
+        this.set('isHidden', true);
+        this.get('flashMessages').success('Added Highlight to Favorites!');
+      })
+      .catch(() => {
+        this.get('flashMessages').danger('Failed To Add To Favorites. Please Try again!');
+      });
+    },
+  }
 });

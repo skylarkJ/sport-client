85d27ded4658b5affccaec0be2f93bd1990a1fac
skylarkJ <janaryndin@gmail.com>
2017-05-23 18:04:57 -0400

    Add dist/

--- app/models/favorite.js
+++
@@ -1,8 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-  title: DS.attr('string'),
-  date: DS.attr('string'),
-  video_id: DS.attr('string'),
-  image: DS.attr('string')
-});
